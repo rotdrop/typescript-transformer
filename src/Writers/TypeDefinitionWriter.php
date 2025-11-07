8fd1c2eaed9f66fa57eb362db5a0abe5dea4fc3f
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-11-07 01:20:38 +0100

    TypeDefinitionWriter, add indentation.

--- src/Writers/TypeDefinitionWriter.php
+++ src/Writers/TypeDefinitionWriter.php
@@ -22,7 +22,7 @@ class TypeDefinitionWriter implements Writer
             $output .= "declare namespace {$namespace} {".PHP_EOL;
 
             $output .= join(PHP_EOL, array_map(
-                fn (TransformedType $type) => "export {$type->toString()}",
+                fn (TransformedType $type) => "  export {$type->toString()}",
                 $types
             ));
 
